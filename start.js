abc695c81215b0b476bc5d4d1723163b2e104c77
Mohsin <mdmohsinhaider6@gmail.com>
2022-11-19 17:42:59 +0530

    Digital clock init

---
+++ start.js
@@ -0,0 +1,21 @@
+function displayClock(){
+    const timE=new Date();
+    const hour_S=timE.getHours();
+    const minute_S=timE.getMinutes();
+    const second_S=timE.getSeconds();
+    const Meridiem=document.getElementById('Meridiem')
+
+    // condition for AM & PM implementation
+        if(hour_S>=12){
+            Meridiem.innerHTML='PM';
+        }
+        else{
+            Meridiem.innerHTML='PM';
+        }
+
+    // implementation variables
+        document.getElementById('hours').innerHTML=hour_S;
+        document.getElementById('minutes').innerHTML=minute_S;
+        document.getElementById('seconds').innerHTML=second_S;
+}
+setInterval(displayClock,10)
